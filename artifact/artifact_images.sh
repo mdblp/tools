6dac59dab05b81438da0cd5828907ddd08df5fa5
franck <franck@diabeloop.fr>
2018-09-17 16:08:37 +0200

    Add diabeloop images and script so that it can be called externally.

---
+++ artifact/artifact_images.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -eu
+
+# get diabeloop icon
+
+wget -q -O favicon.ico 'https://raw.githubusercontent.com/mdblp/tools/dbl/artifact/images/favicon.ico'
+
+wget -q -O app/components/navbar/images/diabeloop/logo.png 'https://raw.githubusercontent.com/mdblp/tools/dbl/artifact/images/logo.png'
