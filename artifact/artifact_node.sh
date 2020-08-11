95a2068f2210f8a51f6695a5c1b7dc22c4b3048c
Fabrice Sougey - Diabeloop <fabrice@diabeloop.fr>
2020-08-11 14:04:20 +0200

    Implement push docker to pictime for node

--- artifact/artifact_node.sh
+++ artifact/artifact_node.sh
@@ -1,81 +1,106 @@
 #!/bin/bash -eu
 
-# Print some variables, so we can debug this script if something goes wrong
-echo "ARTIFACT_NODE_VERSION: ${ARTIFACT_NODE_VERSION}"
-echo "TRAVIS_NODE_VERSION: ${TRAVIS_NODE_VERSION}"
-echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
-echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
-echo "TRAVIS_TAG: ${TRAVIS_TAG}"
-echo "TRAVIS_REPO_SLUG: ${TRAVIS_REPO_SLUG}"
+pushDocker() {
+    # $1 = docker registry
+    # $2 = registry username
+    # $3 = registry password
+    # $4 = image repository
+    # $5 = image tag
+    IMAGE_NAME="$1/$4:$5"
+    echo "Tag image $4 to ${IMAGE_NAME}"
+    docker tag "$4" "${IMAGE_NAME}"
+    echo "Login, push and logout"
+    echo "$3" | docker login --username "$2" --password-stdin $1
+    docker push "${IMAGE_NAME}"
+    docker logout $1
+}
 
-if [ "${TRAVIS_NODE_VERSION}" != "${ARTIFACT_NODE_VERSION}" ]; then
-    exit 0
-fi
+main() {
+    # Print some variables, so we can debug this script if something goes wrong
+    echo "ARTIFACT_NODE_VERSION: ${ARTIFACT_NODE_VERSION}"
+    echo "TRAVIS_NODE_VERSION: ${TRAVIS_NODE_VERSION}"
+    echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
+    echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
+    echo "TRAVIS_TAG: ${TRAVIS_TAG}"
+    echo "TRAVIS_REPO_SLUG: ${TRAVIS_REPO_SLUG}"
 
-# If project has set BUILD_OPENAPI_DOC environment variable to true, then we build the openapi doc
-if [ ${BUILD_OPENAPI_DOC:-false} = true ]; then
-    echo "Build documentation"
-    npm run build-doc
-fi
-
-# If project has set BUILD_SOUP environment variable to true, then we build the SOUPs list
-if [ ${BUILD_SOUP:-false} = true ]; then
-    echo "Build SOUPs list"
-    npm run build-soup
-fi
-
-if [ -n "${TRAVIS_TAG:-}" ]; then
-    ARTIFACT_DIR='deploy'
-
-    APP="${TRAVIS_REPO_SLUG#*/}"
-    APP_DIR="${ARTIFACT_DIR}/${APP}"
-    APP_TAG="${APP}-${TRAVIS_TAG}"
-
-    TMP_DIR="/tmp/${TRAVIS_REPO_SLUG}"
-
-    if [ -f '.artifactignore' ]; then
-        RSYNC_OPTIONS='--exclude-from=.artifactignore'
-    else
-        RSYNC_OPTIONS=''
+    if [ "${TRAVIS_NODE_VERSION}" != "${ARTIFACT_NODE_VERSION}" ]; then
+        exit 0
     fi
 
-    rm -rf "${ARTIFACT_DIR}/" "${TMP_DIR}/" || { echo 'ERROR: Unable to delete artifact and tmp directories'; exit 1; }
-    mkdir -p "${APP_DIR}/" "${TMP_DIR}/" || { echo 'ERROR: Unable to create app and tmp directories'; exit 1; }
+    # If project has set BUILD_OPENAPI_DOC environment variable to true, then we build the openapi doc
+    if [ ${BUILD_OPENAPI_DOC:-false} = true ]; then
+        echo "Build documentation"
+        npm run build-doc
+    fi
 
-    ./build.sh || { echo 'ERROR: Unable to build project'; exit 1; }
+    # If project has set BUILD_SOUP environment variable to true, then we build the SOUPs list
+    if [ ${BUILD_SOUP:-false} = true ]; then
+        echo "Build SOUPs list"
+        npm run build-soup
+    fi
 
-    rsync -a ${RSYNC_OPTIONS} . "${TMP_DIR}/${APP_TAG}/" || { echo 'ERROR: Unable to copy files'; exit 1; }
+    if [ -n "${TRAVIS_TAG:-}" ]; then
+        ARTIFACT_DIR='deploy'
 
-    tar -c -z -f "${APP_DIR}/${APP_TAG}.tar.gz" -C "${TMP_DIR}" "${APP_TAG}" || { echo 'ERROR: Unable to create artifact'; exit 1; }
+        APP="${TRAVIS_REPO_SLUG#*/}"
+        APP_DIR="${ARTIFACT_DIR}/${APP}"
+        APP_TAG="${APP}-${TRAVIS_TAG}"
 
-    rm -rf "${TMP_DIR}/"
-fi
+        TMP_DIR="/tmp/${TRAVIS_REPO_SLUG}"
 
-# Build Docker image whatever
-DOCKER_REPO="docker.ci.diabeloop.eu/${TRAVIS_REPO_SLUG#*/}"
-echo "Building docker image"
-docker build --tag "${DOCKER_REPO}" --build-arg npm_token=${nexus_token} .
+        if [ -f '.artifactignore' ]; then
+            RSYNC_OPTIONS='--exclude-from=.artifactignore'
+        else
+            RSYNC_OPTIONS=''
+        fi
 
-if [ ${SECURITY_SCAN:-false} = true ]; then
-    echo "Security scan"
-    # Microscanner security scan on the built image
-    wget -q -O scanDockerImage.sh 'https://raw.githubusercontent.com/mdblp/tools/feature/add_microscanner/artifact/scanDockerImage.sh'
-    chmod +x scanDockerImage.sh
-    MICROSCANNER_TOKEN=${microscanner_token} ./scanDockerImage.sh ${DOCKER_REPO}
-fi
+        rm -rf "${ARTIFACT_DIR}/" "${TMP_DIR}/" || { echo 'ERROR: Unable to delete artifact and tmp directories'; exit 1; }
+        mkdir -p "${APP_DIR}/" "${TMP_DIR}/" || { echo 'ERROR: Unable to create app and tmp directories'; exit 1; }
 
-# Publish docker image only when we have a tag.
-# To avoid publishing 2x (on the branch build + PR) do not do it on the PR build.
-if [ -n "${TRAVIS_TAG}" -a "${TRAVIS_PULL_REQUEST:-false}" == "false" ]; then
-    # Publish Docker image
-    DOCKER_TAG=${TRAVIS_TAG/dblp./}
+        ./build.sh || { echo 'ERROR: Unable to build project'; exit 1; }
 
-    echo "Docker login"
-    echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin ${DOCKER_REPO}
+        rsync -a ${RSYNC_OPTIONS} . "${TMP_DIR}/${APP_TAG}/" || { echo 'ERROR: Unable to copy files'; exit 1; }
 
-    echo "Tag and push image to ${DOCKER_REPO}:${DOCKER_TAG}"
-    docker tag "${DOCKER_REPO}" "${DOCKER_REPO}:${DOCKER_TAG}"
-    docker push "${DOCKER_REPO}:${DOCKER_TAG}"
-else
-    echo "Not a tag or pull request, not pushing the docker image"
-fi
+        tar -c -z -f "${APP_DIR}/${APP_TAG}.tar.gz" -C "${TMP_DIR}" "${APP_TAG}" || { echo 'ERROR: Unable to create artifact'; exit 1; }
+
+        rm -rf "${TMP_DIR}/"
+    fi
+
+    # Build Docker image whatever
+    DOCKER_REPO="${TRAVIS_REPO_SLUG#*/}"
+
+    echo "Build docker image ${DOCKER_REPO}"
+    docker build --tag "${DOCKER_REPO}" --build-arg npm_token=${nexus_token} .
+
+    if [ ${SECURITY_SCAN:-false} = true ]; then
+        echo "Security scan"
+        # Microscanner security scan on the built image
+        wget -q -O scanDockerImage.sh 'https://raw.githubusercontent.com/mdblp/tools/feature/add_microscanner/artifact/scanDockerImage.sh'
+        chmod +x scanDockerImage.sh
+        MICROSCANNER_TOKEN=${microscanner_token} ./scanDockerImage.sh ${DOCKER_REPO}
+    fi
+
+    # Push docker image only when we have a tag
+    # To avoid publishing 2x (on the branch build + PR) we do not do it on the PR build
+    if [ -n "${TRAVIS_TAG}" -a "${TRAVIS_PULL_REQUEST:-false}" == "false" ]; then
+        # This below removes "dblp." from the TRAVIS_TAG
+        DOCKER_TAG=${TRAVIS_TAG/dblp./}
+        echo "Push image to Diabeloop registry"
+        DOCKER_REGISTRY="docker.ci.diabeloop.eu"
+        pushDocker ${DOCKER_REGISTRY} ${DOCKER_USERNAME} ${DOCKER_PASSWORD} ${DOCKER_REPO} ${DOCKER_TAG}
+
+        # We push to Pictime except if the service does not want to and if we don't have a tag for release candidate
+        if [[ ${PUSH_DOCKER_PICTIME:-true} == "true" && ! ${TRAVIS_TAG} =~ rc[0-9] ]]; then
+            echo "Push image to Pictime registry"
+            DOCKER_REGISTRY="registry.coreye.fr/diabeloop/artifacts/diabeloop_docker"
+            pushDocker ${DOCKER_REGISTRY} ${PCT_DOCKER_USERNAME} ${PCT_DOCKER_PASSWORD} ${DOCKER_REPO} ${DOCKER_TAG}
+        else
+            echo "Skipping push on Pictime registry"
+        fi
+    else
+        echo "Not a tag or pull request, not pushing the docker image"
+    fi
+}
+
+main
